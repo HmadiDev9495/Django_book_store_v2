1aa8069d38ee857eece2b8bf01e8d576d5dfaebc
HmadiDev9495 <amirhoseinhamdi89@gmail.com>
2026-01-01 19:18:23 +0330

    Current complete project - ready for push

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import current_datetime, current_datetime2
-from .views import current_datetime, current_datetime2, book_list_api
+from .views import current_datetime, book_list_api , book_insert
 urlpatterns = [
     path('book-view/', current_datetime),
-    path('book-view2/', current_datetime2),
+    path('book/insert/', book_insert, name='book_insert'),
     path('api/books/', book_list_api, name='book_list_api'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-import datetime
+import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .models import Book
 
 def current_datetime(request):
@@ -26,3 +27,37 @@ def book_list_api(request):
     ))
 
     return JsonResponse(data, safe=False)
+
+
+
+@csrf_exempt
+def book_insert(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+
+            if not data.get('name'):
+                return JsonResponse({"status": "error", "message": "Name is required"}, status=400)
+
+            book = Book.objects.create(
+                name=data.get('name'),
+                author=data.get('author', ''),
+                published_date=data.get('published_date'),
+                price=data.get('price'),
+                currency=data.get('currency'),
+                category=data.get('category'),
+                page_count=data.get('page_count'),
+                publisher=data.get('publisher', ''),
+                description=data.get('description', '')
+            )
+
+            return JsonResponse({
+                "status": "success",
+                "message": "Book added successfully",
+                "book_id": book.id
+            }, status=201)
+
+        except Exception as e:
+            return JsonResponse({"status": "error", "message": str(e)}, status=400)
+
+    return JsonResponse({"status": "error", "message": "Only POST allowed"}, status=405)
